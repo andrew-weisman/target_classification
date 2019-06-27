c71028afc38e7a28e3f0725bd08f118d6ed7f381
Andrew Weisman <andrew.weisman@nih.gov>
2019-06-27 19:42:37 -0400

    Made the initial scraping code a bit more clear by creating a local directory variable and made it work on the Mac

--- scrape.sh
+++ scrape.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 get_dir() {
-    echo $1
-    subdirs=$(curl https://target-data.nci.nih.gov/$1 2> /dev/null | awk -v RS=" " '{print}' | grep "^href=" | awk '{split($0,arr1,"href=\""); split(arr1[2],arr2,"\""); print arr2[1]}' | grep -v "^/\|^?\|^http" | sort -u | grep "/$")
+    local dir=$1
+    echo $dir
+    subdirs=$(curl https://target-data.nci.nih.gov/$dir 2> /dev/null | awk -v RS=" " '{print}' | grep "^href=" | awk '{split($0,arr1,"href=\""); split(arr1[2],arr2,"\""); print arr2[1]}' | egrep -v "^/|^\?|^http" | sort -u | grep "/$")
     for subdir in $subdirs; do
-        echo $1$subdir | grep "/VisCap_Female_Somatic/$\|/VisCap_Male_Somatic/$" > /dev/null
-        if [ ! $? -eq 0 ]; then
-            get_dir $1$subdir
+        #echo $dir$subdir | egrep "/VisCap_Female_Somatic/$|/VisCap_Male_Somatic/$" > /dev/null
+        #if [ ! $? -eq 0 ]; then
+        if [ $? -eq 0 ]; then
+            get_dir $dir$subdir
+        else
+            echo $dir$subdir
         fi
     done
 }
