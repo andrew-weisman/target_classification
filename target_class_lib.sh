129b6d8f941bafc011d1de168cd6db073d714e46
Andrew Weisman <andrew.weisman@nih.gov>
2020-04-27 18:29:56 -0400

    Implemented working downloading function, and started framework for extracting the data from the downloaded files

--- target_class_lib.sh
+++ target_class_lib.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Run this script like:
+# Note documentation for this library is at https://collaborate.nci.nih.gov/x/kJHrDg
+
+# Run this script like, e.g.:
 #
 #   (source /data/BIDS-HPC/private/projects/dmi/checkout/target_class_lib.sh; get_file_placeholders "https://target-data.nci.nih.gov/" "Controlled/ Public/" "/data/BIDS-HPC/private/projects/dmi/data/" |& tee -a all_dirs.txt)
 #   (source /data/BIDS-HPC/private/projects/dmi/checkout/target_class_lib.sh; get_file_placeholders "https://target-data.nci.nih.gov/Public/RT/mRNA-seq/" "L3/ METADATA/" "/data/BIDS-HPC/private/projects/dmi/data/" |& tee -a all_dirs.txt)
+#   (source /data/BIDS-HPC/private/projects/dmi/checkout/target_class_lib.sh; download_files "https://target-data.nci.nih.gov/" "/data/BIDS-HPC/private/projects/dmi/data/" "/home/weismanal/notebook/2020-04-08/scraping_target_site/all_files_in_tree.txt" |& tee -a downloads.txt)
 #
 
 # Main recursive function to process all the directories and files at the input URL; probably don't call this function directly
@@ -38,7 +41,7 @@ crawl() {
         # Create the file index.html in the directory $dir
         url="$url$dir"
         if [ ! -f "$index_file" ]; then
-            wget "$url" 2>> "$datadir/wget_err.txt" || (echo "ERROR: Problem requesting file '$index_file' from URL '$url'"; exit 2)
+            wget "$url" 2>> "$datadir/wget_err-crawl.txt" || (echo "ERROR: Problem requesting file '$index_file' from URL '$url'"; exit 2)
         else
             echo "WARNING: File '$dir$index_file' from URL '$url' already exists"
         fi
@@ -77,10 +80,10 @@ crawl() {
 # Main function to call directly in order to create the file placeholders
 get_file_placeholders() {
 
-    # Sample calls
-    # get_file_placeholders "https://target-data.nci.nih.gov/" "Controlled/ Public/" "/data/BIDS-HPC/private/projects/dmi/data/"
-    # get_file_placeholders "https://target-data.nci.nih.gov/Controlled/AML/mRNA-seq/L3/structural/" "BCCA/ NCI-Meerzaman/" "/data/BIDS-HPC/private/projects/dmi/data/"
-    # get_file_placeholders "https://target-data.nci.nih.gov/Public/RT/mRNA-seq/" "L3/ METADATA/" "/data/BIDS-HPC/private/projects/dmi/data/"
+    # Sample calls:
+    #   get_file_placeholders "https://target-data.nci.nih.gov/" "Controlled/ Public/" "/data/BIDS-HPC/private/projects/dmi/data/"
+    #   get_file_placeholders "https://target-data.nci.nih.gov/Controlled/AML/mRNA-seq/L3/structural/" "BCCA/ NCI-Meerzaman/" "/data/BIDS-HPC/private/projects/dmi/data/"
+    #   get_file_placeholders "https://target-data.nci.nih.gov/Public/RT/mRNA-seq/" "L3/ METADATA/" "/data/BIDS-HPC/private/projects/dmi/data/"
 
     # Parameters
     base_url=$1
@@ -105,6 +108,7 @@ get_file_placeholders() {
     echo "Inputs:"
     echo "  base_url: $base_url"
     echo "  top_directories: $top_directories"
+    echo "  datadir: $datadir"
     echo "Working directory: $working_dir"
     crawl "$base_url" "$top_directories" "$datadir"
     echo "End date/time on $(hostname): $(date)"
@@ -116,5 +120,83 @@ get_file_placeholders() {
 
     # Go back into the original working directory
     cd "$working_dir" || (echo "ERROR: Cannot cd into directory '$working_dir'"; exit 6)
+
+    # Print out the likely next command we'd want to run
+    echo "Likely next command to run:"
+    echo "  download_files \"$base_url\" \"$datadir\" \"$working_dir/all_files_in_tree.txt\""
+
+}
+
+
+# Main function to call directly in order to download the appropriate files from the TARGET site
+download_files() {
+
+    # Sample call:
+    #   download_files "https://target-data.nci.nih.gov/" "/data/BIDS-HPC/private/projects/dmi/data/" "/home/weismanal/notebook/2020-04-08/scraping_target_site/all_files_in_tree.txt"
+
+    # Parameters
+    base_url=$1
+    datadir=$2
+    file_index=$3
+
+    # Append "tree" to the data directory
+    datadir="${datadir}tree/"
+
+    # Get the list of files to download from the full list of indexed files
+    datafiles=$(grep "/L3/" "$file_index" | awk -v FS="____" '{print $1}' | grep -E ".*\.gene\.quantification\.txt$|.*\.gene\.fpkm\.txt$|chla\.org_NBL\.HumanExon\.Level-3\.BER\..*_gene\..*\.txt$|.*\.expression\.txt$" | grep -iv "/archive" | awk '{sub(/^\.\//,""); printf("%s\n",$0)}' | sort -u)
+
+    # For each file to download...
+    for datafile in $datafiles; do
+
+        # Determine the local filename to download, the weblink where it's located, and the current placeholder file (commented out)
+        filename="$datadir${datafile}"
+        weblink="$base_url$datafile"
+        #ls "${filename}____"* # this will get the placeholder file
+
+        # If the downloaded file doesn't already exist, download it; otherwise, print a warning that it isn't being downloaded
+        if [ ! -f "$filename" ]; then
+            echo "Downloading file '$filename'"
+            wget "$weblink" --output-document="$filename" 2>> "wget_err-download.txt"
+        else
+            echo "WARNING: File '$filename' already exists; skipping download"
+        fi
+    done
+
+}
+
+
+# Main function to call directly in order to extract the data into a CSV file
+extract_data() {
+
+    # Sample call:
+    #   extract_data "https://target-data.nci.nih.gov/" "/data/BIDS-HPC/private/projects/dmi/data/" "/home/weismanal/notebook/2020-04-08/scraping_target_site/all_files_in_tree.txt"
+
+    # Parameters
+    base_url=$1
+    datadir=$2
+    file_index=$3
+
+    # Append "tree" to the data directory
+    datadir="${datadir}tree/"
+
+    # Get the list of files that should have been downloaded using the full list of indexed files
+    datafiles=$(grep "/L3/" "$file_index" | awk -v FS="____" '{print $1}' | grep -E ".*\.gene\.quantification\.txt$|.*\.gene\.fpkm\.txt$|chla\.org_NBL\.HumanExon\.Level-3\.BER\..*_gene\..*\.txt$|.*\.expression\.txt$" | grep -iv "/archive" | awk '{sub(/^\.\//,""); printf("%s\n",$0)}' | sort -u)
+
+    # For each datafile...
+    for datafile in $datafiles; do
+
+        # Determine the local filename, the weblink where it's theoretically located, and the current placeholder file (commented out)
+        filename="$datadir${datafile}"
+        weblink="$base_url$datafile"
+        #ls "${filename}____"* # this will get the placeholder file
+
+        # If the downloaded file doesn't exist, exit with an error; otherwise, extract its data
+        if [ ! -f "$filename" ]; then
+            echo "ERROR: Datafile '$filename' does not exist"
+            exit 7
+        else
+            ls "$filename"
+        fi
+    done
 
 }
