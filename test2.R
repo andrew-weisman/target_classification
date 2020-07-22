b479c9bcd14ac1014620379d24f545ec5880d0dd
Andrew Weisman <andrew.weisman@nih.gov>
2020-07-22 01:36:16 -0400

    Finished initial modifications of vst and rlog unsupervised and supervised functions

--- test2.R
+++ test2.R
@@ -14,32 +14,39 @@ DESeqDataSetFromPython <- function(cts_file, anno_file) {
 
 
 rlogUnsupervised <- function(dds) {
-    rld <- rlog(dds)
-    dists <- dist(t(assay(rld)))
-    plot(hclust(dists))
+
+    #rld <- rlog(dds) # original example
+    rld <- rlog(dds, blind=FALSE) # my version makes it not blind
+
+    #plot(hclust(dist(t(assay(rld))))) # original example
+    plotPCA(rld, intgroup="condition") # my version runs PCA instead of plotting a distance matrix
 }
 
 
 vstUnsupervised <- function(dds) {
-    vsd <- varianceStabilizingTransformation(dds)
-    dists <- dist(t(assay(vsd)))
-    plot(hclust(dists))
+
+    #vsd <- varianceStabilizingTransformation(dds) # original example
+    vsd <- vst(dds, blind=FALSE) # my version uses vst() instead of varianceStabilizingTransformation() and also makes it not blind
+
+    #plot(hclust(dist(t(assay(vsd))))) # original example
+    plotPCA(vsd, intgroup="condition") # my version runs PCA instead of plotting a distance matrix
+
 }
 
 
 rlogSupervised <- function(ddsOld, ddsNew) {
 
-    # run the rlog transformation on one dataset
-    design(ddsOld) <- ~ 1
-    ddsOld <- estimateSizeFactors(ddsOld)
-    ddsOld <- estimateDispersions(ddsOld)
-    rldOld <- rlog(ddsOld, blind=FALSE)
+    # Run the rlog transformation on one dataset
+    #design(ddsOld) <- ~ 1 # from ?vst (at least the webpage) I believe this makes rlog() strictly blind to the experimental design, which we don't want, so I'm commenting it out
+    ddsOld <- estimateSizeFactors(ddsOld) # determine the size factors of ddsOld
+    ddsOld <- estimateDispersions(ddsOld) # determine the dispersions of ddsOld
+    rldOld <- rlog(ddsOld, blind=FALSE) # determine the rlog of ddsOld
 
-    # apply the parameters to a new sample
-    mcols(ddsNew)$dispFit <- mcols(ddsOld)$dispFit
-    betaPriorVar <- attr(rldOld,"betaPriorVar")
-    intercept <- mcols(rldOld)$rlogIntercept
-    rldNew <- rlog(ddsNew, blind=FALSE, intercept=intercept, betaPriorVar=betaPriorVar)
+    # Apply the parameters to a new sample
+    mcols(ddsNew)$dispFit <- mcols(ddsOld)$dispFit # make the dispersion function of ddsNew the same as that of ddsOld
+    betaPriorVar <- attr(rldOld,"betaPriorVar") # determine the beta prior variance of the rlog of ddsOld
+    intercept <- mcols(rldOld)$rlogIntercept # determine the rlog intercept of the rlog of ddsOld
+    rldNew <- rlog(ddsNew, blind=FALSE, intercept=intercept, betaPriorVar=betaPriorVar) # determine the rlog of ddsNew using the beta prior variance and rlog intercept of the rlog of ddsOld
 
     return(rldNew)
 
@@ -48,15 +55,15 @@ rlogSupervised <- function(ddsOld, ddsNew) {
 
 vstSupervised <- function(ddsOld, ddsNew) {
 
-    # learn the dispersion function of a dataset
-    design(ddsOld) <- ~ 1
-    ddsOld <- estimateSizeFactors(ddsOld)
-    ddsOld <- estimateDispersions(ddsOld)
+    # Learn the dispersion function of a dataset
+    #design(ddsOld) <- ~ 1 # from ?vst (at least the webpage) I believe this makes vst() (even though it's not explicitly run?) strictly blind to the experimental design, which we don't want, so I'm commenting it out
+    ddsOld <- estimateSizeFactors(ddsOld) # determine the size factors of ddsOld
+    ddsOld <- estimateDispersions(ddsOld) # determine the dispersions of ddsOld
 
-    # use the previous dispersion function for a new sample
-    ddsNew <- estimateSizeFactors(ddsNew)
-    dispersionFunction(ddsNew) <- dispersionFunction(ddsOld)
-    vsdNew <- varianceStabilizingTransformation(ddsNew, blind=FALSE)
+    # Use the previous dispersion function for a new sample
+    ddsNew <- estimateSizeFactors(ddsNew) # determine the size factors of ddsNew
+    dispersionFunction(ddsNew) <- dispersionFunction(ddsOld) # set the dispersions of ddsNew to those of ddsOld
+    vsdNew <- varianceStabilizingTransformation(ddsNew, blind=FALSE) # determine the VST of ddsNew using the dispersions of ddsOld
 
     return(vsdNew)
 
@@ -68,11 +75,8 @@ library("DESeq2")
 
 # Original workflow
 dds <-- DESeqDataSetFromPython("/data/BIDS-HPC/private/projects/dmi2/data/gene_counts.tsv", "/data/BIDS-HPC/private/projects/dmi2/data/annotation.csv")
-dds <- DESeq(dds)
-vsd <- vst(dds, blind=FALSE)
-rld <- rlog(dds, blind=FALSE)
+dds <- DESeq(dds) # can we omit this line in this block of three lines?
 ntd <- normTransform(dds) # note no blind option; this does log2(count(dds,normalized=TRUE) + 1)
-plotPCA(vsd, intgroup="condition")
 
 # rlog - unsupervised
 dds <- makeExampleDESeqDataSet(m=6,betaSD=1)
