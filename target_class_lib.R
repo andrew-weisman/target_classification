0c8c01702a32dcb8ceb68148610c5c5887a6c7b0
Andrew Weisman <andrew.weisman@nih.gov>
2020-07-24 18:21:14 -0400

    The unsupervised plotting seems to work very well on the sample data

--- target_class_lib.R
+++ target_class_lib.R
@@ -1,3 +1,4 @@
+# Load data exported from Python (see main.ipynb) into a DESeqDataSet object
 DESeqDataSetFromPython <- function(cts_file, anno_file) {
     library("DESeq2")
     cts <- as.matrix(read.csv(cts_file, sep="\t", row.names="gene_id"))
@@ -14,47 +15,68 @@ DESeqDataSetFromPython <- function(cts_file, anno_file) {
 }
 
 
-normUnsupervised <- function(dds) {
+# Plot and save PCA and tSNE plots on the top-ntop-variance genes
+plot_pca_and_tsne <- function(transformed_data, transformation_name, data_dir, intgroup="condition", ntop=500) {
+    # Note this function is based off the function plotPCA() in the DESeq2 package
+
     # Don't worry about supervised normal transformation as for vst and rlog as it's probably not recommended and not easy to just transform to new data using the same transformations
     # all(assay(dds) == counts(dds))
     # all(log2(counts(dds, normalized=TRUE)+1) == assay(ntd))
+
+    # Sample call: plot_pca_and_tsne(ntd, "normal transformation", "/data/BIDS-HPC/private/projects/dmi2/data")
+
+    # Import relevant libraries
     library("DESeq2")
     library("ggplot2")
-    ntd <- normTransform(dds) # note no blind option; this does log2(count(dds,normalized=TRUE) + 1)
-    png("/data/BIDS-HPC/private/projects/dmi2/data/normal_transformation.png", height = 6, width = 6, res = 150, units = 'in')
-    print(plotPCA(ntd, intgroup="condition") + ggtitle("normal transformation"))
+    library("Rtsne")
+
+    # Set the random seed for the tSNE analysis below
+    # set.seed(44)    
+
+    # Determine the top-ntop-variance genes
+    rv <- rowVars(assay(transformed_data)) # take the variances of each row (gene) probably
+    select <- order(rv, decreasing = TRUE)[seq_len(min(ntop, length(rv)))] # take the gene indexes having the top ntop variances over the samples, i.e., the genes that matter most for the sample set
+
+    # Run PCA
+    pca <- prcomp(t(assay(transformed_data)[select,])) # do a principle components analysis on these most important genes (when I was doing it from scratch I was taking $rotation instead of $x, running it on assay(transformed_data) instead of t(assay(transformed_data)), and not taking just the top 500 genes)
+
+    # Get the grouping data
+    intgroup.df <- as.data.frame(colData(transformed_data)[, intgroup, drop = FALSE])
+    group <- if (length(intgroup) > 1) {
+        factor(apply(intgroup.df, 1, paste, collapse = ":"))
+    } else {
+        colData(transformed_data)[[intgroup]]
+    }
+
+    # Plot and save the PCA
+    d <- data.frame(PC1 = pca$x[, 1], PC2 = pca$x[, 2], group = group, intgroup.df, name = colnames(transformed_data))
+    percentVar <- pca$sdev^2/sum(pca$sdev^2)
+    png(paste0(data_dir, "/pca_", tolower(gsub(" ", "_", transformation_name)), ".png"), height = 6, width = 6, res = 150, units = 'in')
+    print(ggplot(data = d, aes_string(x = "PC1", y = "PC2", color = "group")) + 
+        geom_point(size = 3) + xlab(paste0("PC1: ", round(percentVar[1] * 
+        100), "% variance")) + ylab(paste0("PC2: ", round(percentVar[2] * 
+        100), "% variance")) + coord_fixed() + ggtitle(paste0("PCA - ", transformation_name))) # plot(pca$x[,1:2] ,col=colData(dds)$condition) produces something similar I believe
     dev.off()
-}
 
+    # Set the perplexity hyperparameter for tSNE to a reasonable value
+    # “Typical values for the perplexity range between 5 and 50”; from help page: should not be bigger than 3 * perplexity < nrow(X) - 1
+    nsamples <- dim(t(assay(transformed_data)))[1]
+    perpl <- round((min(floor((nsamples-1)/3), 50) + 5) / 2)
 
-rlogUnsupervised <- function(dds) {
-    library("DESeq2")
-    library("ggplot2")
-    #rld <- rlog(dds) # original example
-    rld <- rlog(dds, blind=FALSE) # my version makes it not blind
-    #plot(hclust(dist(t(assay(rld))))) # original example
-    png("/data/BIDS-HPC/private/projects/dmi2/data/rlog_transformation.png", height = 6, width = 6, res = 150, units = 'in')
-    print(plotPCA(rld, intgroup="condition") + ggtitle("rlog transformation")) # my version runs PCA instead of plotting a distance matrix
+    # Run tSNE
+    tsne <- Rtsne(t(assay(transformed_data)[select,]), perplexity=perpl, pca=FALSE, theta=0.0) 
+
+    # Plot and save the tSNE
+    d <- data.frame(AX1 = tsne$Y[, 1], AX2 = tsne$Y[, 2], group = group, intgroup.df, name = colnames(transformed_data))
+    png(paste0(data_dir, "/tsne_", tolower(gsub(" ", "_", transformation_name)), ".png"), height = 6, width = 6, res = 150, units = 'in')
+    print(ggplot(data = d, aes_string(x = "AX1", y = "AX2", color = "group")) +
+        geom_point(size = 3) +
+        xlab("Axis 1") +
+        ylab("Axis 2") +
+        coord_fixed() +
+        ggtitle(paste0("tSNE (perplexity: ", perpl, ") - ", transformation_name))) # this results in the same plot as plot(tsne$Y,col=colData(dds)$condition, asp=1)
     dev.off()
-}
 
-
-vstUnsupervised <- function(dds) {
-    library("DESeq2")
-    library("ggplot2")
-    #vsd <- varianceStabilizingTransformation(dds) # original example
-    vsd <- vst(dds, blind=FALSE) # my version uses vst() instead of varianceStabilizingTransformation() and also makes it not blind
-    #plot(hclust(dist(t(assay(vsd))))) # original example
-    png("/data/BIDS-HPC/private/projects/dmi2/data/variance_stabilizing_transformation.png", height = 6, width = 6, res = 150, units = 'in')
-    print(plotPCA(vsd, intgroup="condition") + ggtitle("variance-stabilizing transformation")) # my version runs PCA instead of plotting a distance matrix
-    dev.off()
-}
-
-
-allUnsupervised <- function(dds) {
-    normUnsupervised(dds)
-    rlogUnsupervised(dds)
-    vstUnsupervised(dds)
 }
 
 
@@ -105,63 +127,3 @@ vstSupervised <- function(ddsOld, ddsNew) {
 # ddsOld <- makeExampleDESeqDataSet(m=6)
 # ddsNew <- makeExampleDESeqDataSet(m=1)
 # vstSupervised(ddsOld, ddsNew)
-
-
-
-
-
-
-
-
-
-plot_pca_and_tsne <- function(transformed_data, intgroup="condition", ntop=500) {
-
-    # Sample call: plot_pca_and_tsne(ntd)
-
-    # Import relevant libraries
-    library("DESeq2")
-    library("ggplot2")
-    library("Rtsne")
-
-    # 
-
-}
-
-
-
-rv <- rowVars(assay(ntd)) # take the variances of each row (gene) probably
-select <- order(rv, decreasing = TRUE)[seq_len(min(ntop, length(rv)))] # take the gene indexes having the top ntop variances over the samples, i.e., the genes that matter most for the sample set
-pca <- prcomp(t(assay(ntd)[select,])) # do a principle components analysis on these most important genes (when I was doing it from scratch I was taking $rotation instead of $x, running it on assay(ntd) instead of t(assay(ntd)), and not taking just the top 500 genes)
-percentVar <- pca$sdev^2/sum(pca$sdev^2)
-
-intgroup.df <- as.data.frame(colData(ntd)[, intgroup, drop = FALSE])
-group <- if (length(intgroup) > 1) {
-    factor(apply(intgroup.df, 1, paste, collapse = ":"))
-} else {
-    colData(ntd)[[intgroup]]
-}
-
-d <- data.frame(PC1 = pca$x[, 1], PC2 = pca$x[, 2], group = group, intgroup.df, name = colnames(ntd))
-ggplot(data = d, aes_string(x = "PC1", y = "PC2", color = "group")) + 
-    geom_point(size = 3) + xlab(paste0("PC1: ", round(percentVar[1] * 
-    100), "% variance")) + ylab(paste0("PC2: ", round(percentVar[2] * 
-    100), "% variance")) + coord_fixed() + ggtitle("PCA - normal transformation") # plot(pca$x[,1:2] ,col=colData(dds)$condition) produces something similar I believe
-
-
-
-
-set.seed(44)
-nsamples <- dim(t(assay(ntd)))[1]
-perpl <- round((min(floor((nsamples-1)/3), 50) + 5) / 2)
-tsne <- Rtsne(t(assay(ntd)[select,]), perplexity=perpl, pca=FALSE, theta=0.0) # “Typical values for the perplexity range between 5 and 50”; from help page: should not be bigger than 3 * perplexity < nrow(X) - 1
-
-
-
-
-d <- data.frame(AX1 = tsne$Y[, 1], AX2 = tsne$Y[, 2], group = group, intgroup.df, name = colnames(ntd))
-ggplot(data = d, aes_string(x = "AX1", y = "AX2", color = "group")) +
-    geom_point(size = 3) +
-    xlab("Axis 1") +
-    ylab("Axis 2") +
-    coord_fixed() +
-    ggtitle(paste0("tSNE (perplexity: ", perpl, ") - normal transformation")) # this results in the same plot as plot(tsne$Y,col=colData(dds)$condition, asp=1)
